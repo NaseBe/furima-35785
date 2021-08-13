be089895e54dd4d924fb1bce38f06a6e1f0694a1
NaseBe <nazebesto@gmail.com>
2021-08-13 17:41:17 +0900

    正規表現を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         validates :nickname, presence: true
-         validates :surname_kanji, presence: true
-         validates :name_kanji, presence: true
-         validates :surname_kana, presence: true
-         validates :name_kana, presence: true
-         validates :birthday, presence: true
+
+  with_options presence: true do
+    validates :nickname
+    validates :surname_kanji, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true, message: "is invalid. Input full-width characters."}
+    validates :name_kanji, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true, message: "is invalid. Input full-width characters."}
+    validates :surname_kana, format: {with: /\A[ァ-ヶー－]+\z/, allow_blank: true, message: "is invalid. Input full-width katakana characters."}
+    validates :name_kana, format: {with: /\A[ァ-ヶー－]+\z/, allow_blank: true, message: "is invalid. Input full-width katakana characters."} 
+    validates :birthday
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX,allow_blank: true, message: 'is invalid. Include both letters and numbers'
+
 end
