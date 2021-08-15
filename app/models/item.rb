afeb5035b3d67ffc9c5110f95b57bd37d8a31272
NaseBe <nazebesto@gmail.com>
2021-08-15 14:01:34 +0900

    itemsテーブルのカラム、バリデーション、アソシエーションを実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,24 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :payment_type
+  belongs_to :prefecture
+  belongs_to :shipment_term
+
+  with_options presence: true do
+    validates :name
+    validates :description
+
+    with_options numericality: { other_than: 1, message: "can't be blank" } do
+      validates :category_id
+      validates :condition_id
+      validates :payment_type_id
+      validates :prefecture_id
+      validates :shipment_term_id
+    end
+
+    validates :price numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, allow_blank: true, message: "is out of setting range" }, format: { with: /\A\d+\z/, allow_blank: true, message: "is invalid. Input half-width characters" }
+  end
 end
