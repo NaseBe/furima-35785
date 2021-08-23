2a459d611d9b16bc9c16cbe17fa2e92a46a1c97c
NaseBe <nazebesto@gmail.com>
2021-08-23 23:31:16 +0900

    リダイレクトを実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,10 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+  before_action :move_to_root
 
 def index
   @order_address = OrderAddress.new
-  @item = Item.find(params[:item_id])
 end
 
 def create
@@ -19,6 +21,14 @@ private
 
 def order_params
   params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+end
+
+def set_item
+  @item = Item.find(params[:item_id])
+end
+
+def move_to_root
+  redirect_to root_path unless user_signed_in? && current_user.id != @item.user_id
 end
 
 end
